5f1934f0fdc2b724caa7b21087c05606acc048c5
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-01-30 06:49:39 +0500

    update

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from .views import get_passport
 
 urlpatterns = [
     path("api/passport/<str:passport_id>/", get_passport, name="get_passport"),
+    path("api/newPassport/<str:name>/<str:surname>/<int:phoneNumber>/")
 ]
--- api/views.py
+++ api/views.py
@@ -4,10 +4,8 @@ from .models import Passport
 
 def get_passport(request, passport_id):
     try:
-        # Modeldan pasport ma'lumotlarini olish
         passport = Passport.objects.get(passport_id=passport_id)
         
-        # Ma'lumotni JSON formatiga aylantirish va qaytarish
         return JsonResponse({
             "name": passport.name,
             "surname": passport.surname,
@@ -17,5 +15,4 @@ def get_passport(request, passport_id):
             "address": passport.address
         })
     except Passport.DoesNotExist:
-        # Agar pasport topilmasa, xatolik xabarini qaytarish
         return JsonResponse({"error": "Passport not found"}, status=404)
